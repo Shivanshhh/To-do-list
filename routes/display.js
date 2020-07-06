86c8d4b727ab4b988f4703b7b6b7a818980eb5cd
Shivanshhh <65827695+Shivanshhh@users.noreply.github.com>
2020-07-07 01:00:47 +0530

    eslint

--- routes/display.js
+++ routes/display.js
@@ -1,32 +1,29 @@
-const express = require('express');
-const mongoose=require('mongoose');
-const router = express.Router();
-const List = require('../models/schema');
-items=[];
+const express = require('express')
+const router = express.Router()
+const List = require('../models/schema')
 
-router.get("/", async (req, res) => {
-    var d= await List.find({});
-    res.render("display", {items:d});
-});
-router.post('/', async (req,res) =>{
-    const newDoc= await List.create(req.body);
-    console.log("DB Working");
-    res.redirect("/");
-});
-router.post("/delete", async (req,res)=>{
-    var j = req.body.itemm;
-    await List.findByIdAndDelete(j);
-    res.redirect('/');
-});
-router.post("/update", async (req,res)=>{
-    var d = req.body.itemm1;
-    await List.findByIdAndUpdate(d, {text:req.body.newtext});
-    res.redirect('/');
-});
+router.get('/', async (req, res) => {
+  var d = await List.find({})
+  res.render('display', { items: d })
+})
 
-module.exports=router;
+router.post('/', async (req, res) => {
+  // eslint-disable-next-line no-unused-vars
+  const newDoc = await List.create(req.body)
+  console.log('DB Working')
+  res.redirect('/')
+})
 
+router.post('/delete', async (req, res) => {
+  var j = req.body.itemm
+  await List.findByIdAndDelete(j)
+  res.redirect('/')
+})
 
+router.post('/update', async (req, res) => {
+  var d = req.body.itemm1
+  await List.findByIdAndUpdate(d, { text: req.body.newtext })
+  res.redirect('/')
+})
 
-
-
+module.exports = router
