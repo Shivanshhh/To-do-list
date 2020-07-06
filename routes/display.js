a640fe56f3c9c1dfb7ea689128606afe068a60a5
Shivanshhh <65827695+Shivanshhh@users.noreply.github.com>
2020-07-07 01:08:45 +0530

    eslint 2

--- routes/display.js
+++ routes/display.js
@@ -3,27 +3,27 @@ const router = express.Router()
 const List = require('../models/schema')
 
 router.get('/', async (req, res) => {
-  var d = await List.find({})
-  res.render('display', { items: d })
+    var d = await List.find({})
+    res.render('display', { items: d })
 })
 
 router.post('/', async (req, res) => {
-  // eslint-disable-next-line no-unused-vars
-  const newDoc = await List.create(req.body)
-  console.log('DB Working')
-  res.redirect('/')
+    // eslint-disable-next-line no-unused-vars
+    const newDoc = await List.create(req.body)
+    console.log('DB Working')
+    res.redirect('/')
 })
 
 router.post('/delete', async (req, res) => {
-  var j = req.body.itemm
-  await List.findByIdAndDelete(j)
-  res.redirect('/')
+    var j = req.body.itemm
+    await List.findByIdAndDelete(j)
+    res.redirect('/')
 })
 
 router.post('/update', async (req, res) => {
-  var d = req.body.itemm1
-  await List.findByIdAndUpdate(d, { text: req.body.newtext })
-  res.redirect('/')
+    var d = req.body.itemm1
+    await List.findByIdAndUpdate(d, { text: req.body.newtext })
+    res.redirect('/')
 })
 
 module.exports = router
